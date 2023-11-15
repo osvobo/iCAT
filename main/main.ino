5bcb0a08aa6da2b86b0db009cfc6cc4d29e8b73f
Ondrej Svoboda <68607250+osvobo@users.noreply.github.com>
2023-11-15 15:49:41 +0100

    231115_FL

--- main/main.ino
+++ main/main.ino
@@ -99,8 +99,8 @@ void setup() {
   uint8_t vid, pid;
   uint8_t temp;
   Wire.begin();
-  Serial.begin(115200);
-  //Serial.begin(650000);
+  Serial.begin(57600);
+  // Serial.begin(150000);
 
   SendMessage("UNO is connected");
 
@@ -159,7 +159,7 @@ void setup() {
   SendMessage("iCat is ready!");
   t = millis();
 
-  SendTemperature(18.54);
+  //SendTemperature(18.54);
 }
 
 // line 123, 391
@@ -305,7 +305,7 @@ void myCAMSendToSerial(ArduCAM myCAM) {
   static int k = 0;
   uint8_t temp = 0, temp_last = 0;
   uint32_t length = 0;
-  bool is_header = false;
+  bool jpeg_finished = false;
 
   myCAM.flush_fifo();       //Flush the FIFO
   myCAM.clear_fifo_flag();  //Clear the capture done flag
@@ -316,65 +316,48 @@ void myCAMSendToSerial(ArduCAM myCAM) {
 
   length = myCAM.read_fifo_length();
 
-  snprintf(logMessage, sizeof(logMessage), "Sending image, fifoLength: %d", length);
+  snprintf(logMessage, sizeof(logMessage), "Sending image, fifoLength: %lu", length);
   SendMessage(logMessage);
 
-  if (length >= MAX_FIFO_SIZE)  //8M
+  if (length >= 0xffff)  //8M
   {
-    //Serial.println(F("Over size"));
+    SendMessage("Camera capture - over size");
     return;
   }
   if (length == 0)  //0 kb
   {
-    //Serial.println(F("Size is 0"));
+    SendMessage("Camera capture - size is 0");
     return;
   }
+
+  // Send header through serial line
+  writeToSerialHeader(MSG_TYPE_IMAGE, length);
 
   myCAM.CS_LOW();
   myCAM.set_fifo_burst();
 
-  writeToSerialHeader(MSG_TYPE_IMAGE, length*5);
-
-  while (length--) {
+  while (length > 0)
+  {
+    // if (!jpeg_finished) 
+    // {
     temp_last = temp;
     temp = SPI.transfer(0x00);
 
-    //Read JPEG data from FIFO
+    Serial.write(&temp, 1);
+    length--;
+
+    // Handle scenario where we reached end of JPEG - 0xFF 0xD9
     if ((temp == 0xD9) && (temp_last == 0xFF))  //If find the end ,break while,
     {
-      buf[i++] = temp;  //save the last  0XD9
-      //Write the remain bytes in the buffer
-      myCAM.CS_HIGH();
-
-      for (int i = 0; i < sizeof(buf); i++) {
-        Serial.write(buf[i]);
+      while (length > 0) {
+        Serial.write(0);
+        length--;
       }
-
-      is_header = false;
-      i = 0;
     }
 
-    if (is_header == true) {
-      //Write image data to buffer if not full
-      if (i < 5) {
-        buf[i++] = temp;
-      } else {
-        //Stream 5 bytes of raw image data to serial
-        myCAM.CS_HIGH();
-        for (int i = 0; i < sizeof(buf); i++) {
-          Serial.write(buf[i]);
-        }
-        i = 0;
-        buf[i++] = temp;
-        myCAM.CS_LOW();
-        myCAM.set_fifo_burst();
-      }
-    } else if ((temp == 0xD8) & (temp_last == 0xFF)) {
-      is_header = true;
-      buf[i++] = temp_last;
-      buf[i++] = temp;
-    }
   }
+
+  myCAM.CS_HIGH();
 }
 
 
